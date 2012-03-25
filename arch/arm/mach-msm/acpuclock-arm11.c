50ffa6fc16cdf86e4f4f5b873fe6a501ee0d99d6
bluewater-cn <4981046@qq.com>
2012-03-25 10:21:18 +0800

    update latte-config and not oc for device stable

--- arch/arm/mach-msm/acpuclock-arm11.c
+++ arch/arm/mach-msm/acpuclock-arm11.c
@@ -182,17 +182,12 @@ static unsigned long max_axi_rate;
 #ifdef CONFIG_CPU_FREQ
 static struct cpufreq_frequency_table *freq_table;
 static struct cpufreq_frequency_table msm7227_freq_table[] = {
-	{ 0, 245760 },
-	{ 1, 400000 },
-	{ 2, 480000 },
-	{ 3, 600000 },
-	{ 4, 768000 },
-	{ 5, 787200 },
-	{ 6, 806400 },
-	{ 7, 825600 },
-	{ 8, 844800 },
-	{ 9, 864000 },
-	{ 10, CPUFREQ_TABLE_END },
+	{ 0, 122880 },
+	{ 1, 245760 },
+	{ 2, 400000 },
+	{ 3, 480000 },
+	{ 4, 600000 },
+	{ 5, CPUFREQ_TABLE_END },
 };
 static struct cpufreq_frequency_table msm72xx_freq_table[] = {
 #if defined(CONFIG_TURBO_MODE)
